52847d9f6172457d42119793b524e5e8a9b41d2f
Ryan Lee <ryanlee7132@gmail.com>
2020-04-01 13:30:55 -0400

    Update SendText.py

--- SendText.py
+++ SendText.py
@@ -1,12 +1,12 @@
 from twilio.rest import Client
 
-account_sid = 'ACfc288a2abc8e485d8383bcd0dbb23a8d'
-auth_token = '6123cb974bc495c71b2bbe86d5245793'
+account_sid = 'hidden'
+auth_token = 'hidden'
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
                               body=priceData,
-                              from_='+15208679675',
+                              from_='hidden',
                               to=userNumber
                           )
 
